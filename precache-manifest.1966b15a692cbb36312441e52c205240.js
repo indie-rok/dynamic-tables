f05b7e4021fb84772bd77bae3bdc3cb63a78bbcf
Emmanuel Orozco <emmanuel.orozco@wildcodeschool.com>
2021-09-01 21:00:46 +0200

    Updates

---
+++ precache-manifest.1966b15a692cbb36312441e52c205240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491ae79386972373c133c4958e85d3fb",
+    "url": "/dynamic-tables/index.html"
+  },
+  {
+    "revision": "0023aba975bf98c9467e",
+    "url": "/dynamic-tables/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c2ef86edaf1de73ebc7",
+    "url": "/dynamic-tables/static/js/2.2a81dafe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dynamic-tables/static/js/2.2a81dafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0023aba975bf98c9467e",
+    "url": "/dynamic-tables/static/js/main.28fcdab8.chunk.js"
+  },
+  {
+    "revision": "dfad8414797f4ee30882",
+    "url": "/dynamic-tables/static/js/runtime-main.4f06d7c4.js"
+  },
+  {
+    "revision": "bd65acdc92d96c88d79efa96998a4ccb",
+    "url": "/dynamic-tables/static/media/icon-plus-with-circle.bd65acdc.svg"
+  }
+]);
